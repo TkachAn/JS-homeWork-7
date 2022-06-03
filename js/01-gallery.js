51b704470862516156beb7c04b04cb0d00166731
Andrey Tkachenko <61586895+TkachAn@users.noreply.github.com>
2022-06-03 16:36:18 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,72 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const bodyPrew = document.querySelector('body');
 const galleryPrew = document.querySelector('.gallery');
-
-// const modal = document.querySelector('.backdrop');
+console.log(bodyPrew);
 const items = [];
-const preview = [];
-const original = [];
-const description = [];
-// let outImage;
-
-for (const item of galleryItems) {
-  preview.push(item.preview);
-	original.push(item.original);
-	description.push(item.description);
-}
 
 for(let i = 0; i < 9; i++){
+	const itemDiv = document.createElement('div');
+		itemDiv.classList.add('gallery__item');
 
-const itemDiv = document.createElement('div');
-itemDiv.classList.add('gallery__item');
+	const itemA = document.createElement('a');
+		itemA.classList.add('gallery__link');
+		itemA.href = galleryItems[i].original;	
 
-const itemA = document.createElement('a');
-itemA.classList.add('gallery__link');
-itemA.href = original[i];	
+	const itemImg = document.createElement('img');
+		itemImg.classList.add('gallery__image'); 
+		itemImg.src = galleryItems[i].preview;
+		itemImg.alt = galleryItems[i].description;
+		itemImg.dataset.num = `${i}`
 
-const itemImg = document.createElement('img');
-itemImg.classList.add('gallery__image'); 
-itemImg.src = preview[i];
-itemImg.alt = description[i];
-itemImg.dataset.num = `${i}`
-
-itemA.appendChild(itemImg);
-itemDiv.appendChild(itemA);
-items.push(itemDiv);
+	itemA.appendChild(itemImg);
+	itemDiv.appendChild(itemA);
+	items.push(itemDiv);
 };
 
 galleryPrew.append(...items);
 
-// galleryPrew.addEventListener('click', 'gallery__link');
-// e.preventDefault();
-const links = document.querySelectorAll('.gallery__link');
-console.log(links);
+// galleryPrew.addEventListener("Escape", console.log('close'));
+galleryPrew.addEventListener("click", basLb);
 
-
-links.forEach(function callback(link) {
-  link.addEventListener("click", function(event) {
-		console.log("Мы отменили стандартное действие браузера");
-		event.preventDefault();
-	}, false);
-});
-
-
-galleryPrew.addEventListener("click", (event) => {
+function basLb(event){
 	if (event.target.nodeName !== 'IMG') {
 		return;
   }
-	// console.log(event.target);
+	event.preventDefault();
 	const index = Number(event.target.dataset.num);
-	// console.log(index); basicLightbox--visible
-	basicLightbox.create(`<img src = ${original[index]}>`).show();
-	
-	
-});
-console.log('event.target');
-
-const modal = document.querySelector('.basicLightbox--visible');
-console.log(modal);
-// modal.addEventListener('Escape', esc);
-// 	function esc(){console.log('close();');
-		
-// 	}
+	basicLightbox.create(`<img src = ${galleryItems[index].original}>`).show();
+}
